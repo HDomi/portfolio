71f1ac3d59d4ed5567d16c50675cf68ff2f6d311
HDomi <hwangjae1139@gmail.com>
2022-07-07 17:26:45 +0900

    220707 update 3

--- commit.bat
+++ commit.bat
@@ -3,9 +3,10 @@ color 0b
 :Home
 git status
 echo Go commit?
-set /p go=Press Enter :
+set /p go=*See your files, Press Enter* :
 goto run
 setlocal
+pause
 
 :run
 set /p commit= *Commit TXT* :
--- src/pages/cnts/cnttest.js
+++ src/pages/cnts/cnttest.js
@@ -6,7 +6,7 @@ const CntTest =() => {
     return(
         <div className="cnt_wrap">
                 <div className="cnt">
-                  <div className="cnt_tit">테스트페이지1</div>
+                  <div className="cnt_tit">테스트페이지11111</div>
                   <div className="tit_line"></div>
                   <div className="cnt_sub_tit">서브타이틀</div>
                   <div className="cnt_sec">
