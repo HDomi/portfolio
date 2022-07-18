bbb9b159051565e86189d8c5c0886968cdd1d4b4
HDomi <hwangjae1139@gmail.com>
2022-07-18 15:03:24 +0900

    220718 update 2

--- src/pages/design_bn.js
+++ src/pages/design_bn.js
@@ -5,14 +5,16 @@ class Desingbn extends Component {
     
     render() {
         const menus = [
-            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"
+            "1", "2", "3", "4",
+            "5", "6", "7", "8",
+            "9", "10", "11", "12"
         ]
         const menuList = menus.map((menu, index) => (
-            <div className="dg_cnt_wrap" key={index}>
+            <div className="dg_cnt_wrap" key={index} onClick={() => window.open(process.env.PUBLIC_URL + '/img/banner/' + index + '.jpg', '_blank', 'height=300px', 'width=760px')}>
                 <div className="dg_cnt">
                     <img className="test" alt="배너사진" src={process.env.PUBLIC_URL + '/img/banner/' + index + '.jpg'}/>
                     <div className="dg_cnt_txt">
-                        클릭하여 자세히 보기
+                        클릭하여 새 창으로 보기
                     </div>
                 </div>
             </div>
@@ -23,7 +25,7 @@ class Desingbn extends Component {
             <div className="cnt">
                 <div className="cnt_tit">배너 디자인</div>
                 <div className="tit_line"></div>
-                <div className="cnt_sub_tit">Banner_Design / 이전 작업물 중 일부를 첨부하였습니다.</div>
+                <div className="cnt_sub_tit">Banner_Design / 작업물 중 일부를 첨부하였습니다.</div>
                 <div className="cnt_sec cnt_sec2 design_banner_sec">
                     <div className="cnt_inner_wrap">    
                         {menuList}
