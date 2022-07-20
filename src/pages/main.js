4f1a40ccc4f045e12f6466928cbfab6ef66a3a39
HDomi <hwangjae1139@gmail.com>
2022-07-20 16:55:52 +0900

    220720 update 5

--- src/pages/main.js
+++ src/pages/main.js
@@ -15,9 +15,16 @@ const Main =() => {
                 <div className="tit_line"></div>
                 <div className="cnt_sub_tit">HJY's Portfolio</div>
                 <div className="cnt_sec main_sec">
+                    <div className="link_area">
+                        제일 먼저, 이전에 만들었던 포트폴리오를 보며 <b>자아성찰</b>을 시작합니다.
+                        <div className="link_txt" onClick={() => window.open('http://hwang1139.cafe24.com/html/portfolio/index.html', '_blank')}>
+                            이전 포트폴리오 바로가기
+                        </div>                       
+                    </div> 
+                
                     <div className="text_area">
-                        포트폴리오를 만들 계획을 하던중,<br/>흔하고 화려한 포트폴리오가 아닌, <b>블로그 같은 친숙하고 깔끔한 디자인</b>을
-                        계획했습니다.<br/><br/> 그리하여 디자인 시작.
+                        다시한번 계획 시작,<br/>흔하고 화려한 포트폴리오가 아닌, <b>블로그 같은 친숙하고 깔끔한 디자인</b>을
+                        디자인으로 정했습니다.<br/><br/> 그리하여 디자인 시작.
                     </div>
                     <div dangerouslySetInnerHTML={{ __html: "<iframe style='border: 1px solid rgba(0, 0, 0, 0.1); margin-top:40px; border-radius:8px;' width='100%' height='350px' src='https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FdwJcOkbVxk5F0HqIv46HxW%2FDomi_Page' allowfullscreen/>"}} />
                     
