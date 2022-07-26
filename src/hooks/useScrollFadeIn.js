4012320c1f41e6c102c3c3d8263d08ab370b0080
HDomi <hwangjae1139@gmail.com>
2022-07-26 09:02:54 +0900

    220726 update 1

--- src/hooks/useScrollFadeIn.js
+++ src/hooks/useScrollFadeIn.js
@@ -1,53 +1,45 @@
-import { useRef, useEffect, useCallback } from 'react';
+import { useRef, useCallback, useEffect } from 'react'
 
-const useScrollFadeIn = (direction = 'up', duration = 1, delay = 0) => {
-  const element = useRef();
-
-  const handleDirection = (name) => {
-    switch (name) {
-      case 'up':
-        return 'translate3d(0, 50%, 0)';
-      case 'down':
-        return 'translate3d(0, -50%, 0)';
-      case 'left':
-        return 'translate3d(50%, 0, 0)';
-      case 'right':
-        return 'translate3d(-50%, 0, 0)';
-      default:
+// 스크롤하면 페이드인 하는 애니메이션
+export const useScrollFadeIn = (direction, duration) => {
+// 해당 컴포넌트 가져오기
+const element = useRef()
+// direction 선택
+const handleDirection = (name) => {
+	switch (name) {
+    	case "up":
+        	return "translate3d(0, 50%, 0)"
+        case "down": return "tranlate3d(0, -50%, 0)"
+        case "left": return "translate3d(50%, 0, 0)"
+        case "right": return "translate3d(-50%, 0, 0)"
+        default:
         return;
     }
-  };
+}
 
-  const onScroll = useCallback(
-    ([entry]) => {
-      const { current } = element;
-      if (entry.isIntersecting) {
-        current.style.transitionProperty = 'all';
-        current.style.transitionDuration = `${duration}s`;
-        current.style.transitionTimingFunction = 'cubic-bezier(0, 0, 0.2, 1)';
-        current.style.transitionDelay = `${delay}s`;
+// 설정해둔 컴포넌트를 만날때마다 함수가 재실행되도록 callback하기
+const onScroll = useCallback(([entry])=>{
+	const { current } = element
+    if(entry.isIntersecting) {
+    	current.style.tansitionProperty = 'all'
+        current.style.transitionDuration = `${duration}s`
+        current.style.transitionTimingFunction = "cubic-bezier(0, 0, 0.2, 1)"
         current.style.opacity = 1;
-        current.style.transform = 'translate3d(0, 0, 0)';
-      }
-    },
-    [delay, duration],
-  );
-
-  useEffect(() => {
-    let observer;
-
-    if (element.current) {
-      observer = new IntersectionObserver(onScroll, { threshold: 0.7 });
-      observer.observe(element.current);
+        current.style.tranform = "translate3d(0, 0, 0)"
     }
+}, [duration])
 
-    return () => observer && observer.disconnect();
-  }, [onScroll]);
+// intersection-observer로 컴포넌트 위치 observe하기
+useEffect(()=>{
+	let observer
+    
+    if(element.current) {
+    	observer = new IntersectionObserver(onScroll, {threshold: 0.7})
+        observer.observe(element.current)
+        }
+        
+        return() => observer && observer.disconnect()
+}, [onScroll])
 
-  return {
-    ref: element,
-    style: { opacity: 0, transform: handleDirection(direction) },
-  };
-};
-
-export default useScrollFadeIn;
+return{ref: element, style: { opacity: 0, transform: handleDirection(direction) } }
+}
--- src/pages/cnt_table.js
+++ src/pages/cnt_table.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
     Routes,
     Route,
+    Navigate
   } from "react-router-dom";
 import './cnts/cnt.css';
 import Main from "./main.js";
@@ -16,11 +17,13 @@ function CntTable(){
         <Routes>
             <Route path="/" >
               <Route path="/" element={<Main />} />
+              <Route path="*" element={<Navigate replace to="/portfolio/"/>} />
               <Route path="/portfolio/" element={<Main />} />
               <Route path="/portfolio/profile" element={<Profile />} />
 
               <Route path="/portfolio/wd/banner" element={<Desingbn />} />
               <Route path="/portfolio/wd/Landing" element={<LandingPage />} />
+            
             </Route>
         </Routes>  
     );
