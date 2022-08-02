eaa7081a90f8691ba11b87e7c135d512a4b1fe98
HDomi <hwangjae1139@gmail.com>
2022-08-02 14:19:22 +0900

    220802 update 1

--- src/pages/loading.js
+++ src/pages/loading.js
@@ -16,35 +16,9 @@ const Loading =() => {
         align-items: center;
         justify-content: center;
     `;
-    const LoadingText = styled.div`
-        text-align: center;
-        font-weight:bold;
-        margin-top:90px;
-        color:#080f2a;
-    `;
-    const LoadingText2 = styled.div`
-        text-align: center;
-        font-weight:900;
-        font-size: 24px;
-        margin-top:20px;
-        color:#080f2a;
-    `;
-    const BgInner = styled.div`
-        text-align: center;
-        background: #fff;
-        width: 350px;
-        height: 350px;
-        border-radius: 50%;
-        box-shadow: inset 0px 0px 20px 5px rgba(0,0,0,0.6);
-    `;
-    
     return (
             <Background>
-                <BgInner>
-                    <LoadingText>황재영의 포트폴리오에<br/>오신것을 환영합니다</LoadingText>
-                    <LoadingText2>ㆍㆍㆍ로딩중ㆍㆍㆍ</LoadingText2>
-                    <img src={Spinner} alt="로딩중" width="150px" />
-                </BgInner>  
+                <div className="loading_txt" data-text="Loading...">Loading...</div>
             </Background>
             );
         };
--- src/pages/webpub.js
+++ src/pages/webpub.js
@@ -19,28 +19,48 @@ const Webpub =() => {
             title:"편의",
             old:"2021.06 ~ 06",
             des:"양성교육 과정 중 심플함 속 화려한 색감을 표현하기 좋은 한지를 아이템으로 한 쇼핑몰을 만들어 보고 싶었습니다.",
-            gyd:"100%"
+            gyd:"100%",
+            buh:"X"
         },
         {
             id:1,
             title:"도미넌스",
             old:"2021.07 ~ 07",
             des:"소프트웨어 뿐이 아니라 하드웨어에도 관심이 많아, 컴퓨터를 판매하는 쇼핑몰을 만들어 보았습니다.",
-            gyd:"100%"
+            gyd:"100%",
+            buh:"X"
         },
         {
             id:2,
-            title:"스마트시티 라이브러리",
-            old:"2021.12 ~ 01",
-            des:"정부지원 사업으로 진행되었던 프로젝트",
-            gyd:"100%"
+            title:"스마트 패턴장부 3page",
+            old:"2021.11 ~ 11",
+            des:"기존 런칭중이던 스마트 패턴장부에 3개 페이지와 팝업창을 추가 하는 작업입니다.",
+            gyd:"100%",
+            buh:"O"
         },
         {
             id:3,
+            title:"스마트시티 라이브러리",
+            old:"2021.12 ~ 01",
+            des:"정부지원 사업으로 진행되었던 프로젝트",
+            gyd:"100%",
+            buh:"일부적용"
+        },
+        {
+            id:4,
             title:"데이터가공 서비스",
             old:"2022.04 ~ 04",
-            des:"㈜패턴테크의 첫 런칭 프로젝트로 이미지를 사이트 내에서 쉽게 가공해주는 사이트입니다.",
-            gyd:"100%"
+            des:"이미지를 사이트 내에서 쉽게 가공해주는 사이트입니다.",
+            gyd:"100%",
+            buh:"일부적용"
+        },
+        {
+            id:5,
+            title:"황재영의 포트폴리오",
+            old:"2022.07 ~ 08",
+            des:"황재영의 포트폴리오 입니다. 첫 React 작업물",
+            gyd:"100%",
+            buh:"O"
         }
     ]
     //랜딩 리스트를 누르면 누른 해당 id가져오게 저장
@@ -52,15 +72,15 @@ const Webpub =() => {
         openModal();
     };
     const pubList = webpub_thum.map((item)=>{
-        const {id,title,old,des,gyd} = item
+        const {id,title,old,des,gyd,buh} = item
                 return(
                     <div className="pub_cnt_wrap" key={id}>
                         <div className="pub_cnt" {...animatedItem[id]} onClick={buttonValueSetting}>
                             <div className="webpub_txt" id={id}>
-                                <b>{title}<br/><div className="webpub_gyd">기여도 : {gyd}</div></b><br/>
+                                <b>{title}<br/><div className="webpub_gyd">기여도 : {gyd} / 반응형 : {buh}</div></b><br/>
                                 <p>{old}<br/><br/><span>{des}</span></p>
                             </div>
-                            <img className="webpub_img" alt="배너사진" src={process.env.PUBLIC_URL + '/img/webpub/thumnail/' + id + '.jpg'}/> 
+                            <img className="webpub_img" id={id} alt="배너사진" src={process.env.PUBLIC_URL + '/img/webpub/thumnail/' + id + '.jpg'}/> 
                         </div>
                     </div>
                 )
@@ -102,6 +122,18 @@ const Webpub =() => {
         },
         {
             id:2,
+            title:"스마트 패턴장부",
+            subtit:"기존 런칭중이던 스마트 패턴장부에 3개 페이지와 팝업창 추가",
+            firsttxt:"100%",
+            secondtxt:"기존 진행했던 컨셉이 있기 때문에 해당 톤 앤 매너에 맞춰 디자인",
+            thirdtxt:"SNS로그인, 에러팝업, 메세지 서비스 토글시 메뉴 열림, 세부 날짜 등 팝업으로 선택 등",
+            fourthtxt:"주 대상 이용층이 40~50대 장사하시는 분들이기에 너무 이쁘기만 한 디자인 보다는 직관적이고, 기능을 확실히 알아 볼 수 있게 구성",
+            figmalk:{ __html: "<iframe width='100%' height='100%' src='https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FHU7TI8ByPG5tFXzi0693cR%2F%25EC%258A%25A4%25EB%25A7%2588%25ED%258A%25B8%25EC%259E%25A5%25EB%25B6%2580_%25ED%2599%25A9%25EC%259E%25AC%25EC%2598%2581' allowfullscreen/>"},
+            shortct:"https://hdomi.github.io/smartpr/views/login.html",
+            link:{ __html: "<iframe width='400px' height='800px' src='https://hdomi.github.io/smartpr/views/login.html' allowfullscreen/>"},
+        },
+        {
+            id:3,
             title:"스마트시티 라이브러리",
             subtit:"정부지원 사업으로 진행되었던 프로젝트",
             firsttxt:"100%",
@@ -113,9 +145,9 @@ const Webpub =() => {
             link:{ __html: "<iframe width='100%' height='100%' src='https://hdomi.github.io/smartcity/views/main' allowfullscreen/>"},
         },
         {
-            id:3,
+            id:4,
             title:"데이터가공 서비스",
-            subtit:"㈜패턴테크의 첫 런칭 프로젝트로 이미지를 사이트 내에서 쉽게 가공해주는 사이트",
+            subtit:"이미지를 사이트 내에서 쉽게 가공해주는 사이트",
             firsttxt:"100%",
             secondtxt:"클라이언트 측에서 전달한 레퍼런스를 토대로 활발한 커뮤니케이션을 통해 디자인과 테마 색상을 결정",
             thirdtxt:"이미지 업로드, 요금제 선택, 로그인을 통한 마이페이지 등 여러 기능을 탑재",
